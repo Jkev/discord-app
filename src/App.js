8604ac9aa3539ae8394edafab3307d78abe7d7e7
Jkev <endlezzjkev@gmail.com>
2020-10-21 18:40:57 -0500

    mu-core-icons instaled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h2>discord App</h2>
+      {/* sidebar */}
+      <Sidebar />
+      {/* chat */}
     </div>
   );
 }
